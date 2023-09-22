405e625d3be3f43b4db764526e61b1dde7fbc869
Krishna Prasad Regmi <kpkrishna309@gmail.com>
2023-09-22 15:00:18 +0530

    Add files via upload

---
+++ Pizza Ordering System/myjs.js
@@ -0,0 +1,8 @@
+function exit(){
+    var c = confirm("Do you wnat to exit?");
+    if(c==true)
+        window.close()
+}
+function clear_Price(){
+    document.getElementById("final_Price").value="0.00";
+}
